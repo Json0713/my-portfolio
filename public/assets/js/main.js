2e09124281beceee14f7e4309141a1426d6aa5a8
Json0713 <jason.orsuabayaga13@gmail.com>
2025-06-27 01:32:01 +0800

    refine-manifest-json-deploy

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -15,8 +15,8 @@ document.addEventListener("DOMContentLoaded", () => {
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
       navigator.serviceWorker.register('/service-worker.js')
-        .then(() => console.log('✅ Service Worker registered'))
-        .catch(err => console.error('❌ Service Worker registration failed:', err));
+        .then(() => console.log('Service Worker registered'))
+        .catch(err => console.error('Service Worker registration failed:', err));
     });
   }
 });
