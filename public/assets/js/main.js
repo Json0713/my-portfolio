7a4b6e0aabdf96897fdd94e12fa4953003c06830
Json0713 <jason.orsuabayaga13@gmail.com>
2025-06-28 04:34:50 +0800

    merge-new-features-preview-wpa

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -2,20 +2,41 @@
 
 import { initTheme, setupThemeToggle } from './public/theme.js';
 import { highlightActiveLink } from './routes/routerLink.js';
-import { setupInstallPrompt } from './common/installPrompt.js';
+import { setupInstallPrompt, setupUpdatePrompt } from './common/installPrompt.js';
 
 document.addEventListener("DOMContentLoaded", () => {
   initTheme();
   setupThemeToggle();
   highlightActiveLink();
-  
   setupInstallPrompt();
 
-  // Service Worker registration at root
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
       navigator.serviceWorker.register('/service-worker.js')
-        .then(() => console.log('Service Worker registered'))
+        .then(reg => {
+          console.log('Service Worker registered:', reg.scope);
+
+          if (reg.waiting) {
+            setupUpdatePrompt(reg);
+          }
+
+          reg.addEventListener('updatefound', () => {
+            const newWorker = reg.installing;
+            if (newWorker) {
+              newWorker.addEventListener('statechange', () => {
+                if (newWorker.state === 'installed' && navigator.serviceWorker.controller) {
+                  setupUpdatePrompt(reg);
+                }
+              });
+            }
+          });
+
+          navigator.serviceWorker.addEventListener('message', (event) => {
+            if (event.data?.type === 'NEW_VERSION_AVAILABLE') {
+              setupUpdatePrompt(reg);
+            }
+          });
+        })
         .catch(err => console.error('Service Worker registration failed:', err));
     });
   }
