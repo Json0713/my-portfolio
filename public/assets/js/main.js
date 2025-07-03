d1c0d465cf0cbbfe0f876d47eb15b2168b01abd9
Json0713 <jason.orsuabayaga13@gmail.com>
2025-07-03 16:33:02 +0800

    merge-main-dev-production

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,22 +1,12 @@
 // assets/js/main.js
+import { registerServiceWorker } from './common/swRegister.js';
 
 import { initTheme, setupThemeToggle } from './public/theme.js';
-import { highlightActiveLink } from './routes/routerLink.js';
 import { setupInstallPrompt } from './common/installPrompt.js';
 
 document.addEventListener("DOMContentLoaded", () => {
   initTheme();
   setupThemeToggle();
-  highlightActiveLink();
-  
   setupInstallPrompt();
-
-  // Service Worker registration at root
-  if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/service-worker.js')
-        .then(() => console.log('Service Worker registered'))
-        .catch(err => console.error('Service Worker registration failed:', err));
-    });
-  }
+  registerServiceWorker();
 });
